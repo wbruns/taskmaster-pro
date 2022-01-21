353ade43440c64cf6b3a27e311d0c560bfddc863
wbruns <wbruns07@gmail.com>
2022-01-21 17:31:02 -0600

    used moment.js to check due dates for whether or not they are approaching deadline or overdue, color coded tasks accordingly by adding new classes in the check process. Works for new tasks and edited tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,33 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
+};
+
+// check the due date and color the task accordingly
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  // ensure it worked
+  console.log(date);
+
+  // convert to moment obect at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 var loadTasks = function() {
@@ -101,7 +125,7 @@ $(".list-group").on("click", "span", function() {
   $(this).replaceWith(dateInput);
   // enable jquery ui datepicker
   dateInput.datepicker({
-    minDate: 1,
+     minDate: 1,
     onClose: function() {
       // when calendar is closed, force a "change" event on the 'dateInput'
       $(this).trigger("change");
@@ -135,6 +159,9 @@ $(".list-group").on("change", "input[type='text']", function() {
     .text(date);
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // make lists sortable with jQuery UI
