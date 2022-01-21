f960e212e3a734d364131232d38acf54a3b30eaa
wbruns <wbruns07@gmail.com>
2022-01-21 17:37:23 -0600

    cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,7 +125,7 @@ $(".list-group").on("click", "span", function() {
   $(this).replaceWith(dateInput);
   // enable jquery ui datepicker
   dateInput.datepicker({
-     minDate: 1,
+    minDate: 1,
     onClose: function() {
       // when calendar is closed, force a "change" event on the 'dateInput'
       $(this).trigger("change");
